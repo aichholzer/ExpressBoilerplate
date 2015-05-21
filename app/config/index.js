8bc047ea1614ce6cc2bf6ef0ba48f67f50abd291
Stefan Aichholzer <theaichholzer@gmail.com>
2015-05-21 17:48:39 +0200

    Configuration files and settings. Unstable.

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+module.exports = function config() {
+
+    var configFile = (process.env.NODE_ENV === 'undefined') ? 'development' : process.env.NODE_ENV,
+        config = require('./setups/' + configFile);
+
+    return config;
+};
