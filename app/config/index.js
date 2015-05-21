0752e17154967dc1522fc589c6d0a0e46660dbd0
Stefan Aichholzer <theaichholzer@gmail.com>
2015-05-21 17:50:29 +0200

    Configuration index.js

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function config() {
 
-    var configFile = (process.env.NODE_ENV === 'undefined') ? 'development' : process.env.NODE_ENV,
+    var configFile = process.env.NODE_ENV === 'undefined' ? 'development' : process.env.NODE_ENV,
         config = require('./setups/' + configFile);
 
     return config;
